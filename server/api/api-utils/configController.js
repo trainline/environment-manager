23d729ea52307e37da863f346ffdcc8a0fbbc7dd
Filip Sobczak <Filip.Sobczak@thetrainline.com>
2016-09-29 14:33:50 +0100

    refactor - turn helper into class

--- server/api/api-utils/configController.js
+++ server/api/api-utils/configController.js
@@ -12,89 +12,89 @@ let createValue = require('commands/resources/CreateDynamoResource');
 let deleteValue = require('commands/resources/DeleteDynamoResource');
 let metadata = require('commands/utils/metadata');
 
-/**
- * Get all resources in a Dynamo table
- */
-function getAll(resource) {
-  return getAllValues({ resource, exposeAudit, accountName });
+
+class DynamoHelper {
+
+  constructor(resource) {
+    this.resource = resource;
+  }
+
+  /**
+   * Get all resources in a Dynamo table
+   */
+  getAll() {
+    return getAllValues({ resource: this.resource, exposeAudit, accountName });
+  }
+
+  /**
+   * Get a specific resource from a Dynamo table
+   */
+  getByKey(key) {
+    return getValue({ resource: this.resource, key, exposeAudit, accountName });
+  }
+
+  /**
+   * Get a specific resource from a Dynamo table that has includes sort key
+   */
+  getBySortKey(partitionKey, sortKey) {
+    return getValue({ key: partitionKey, range: sortKey, resource: this.resource, exposeAudit, accountName });
+  }
+
+  /**
+   * Create a resource in a Dynamo table
+   */
+  create(value, keyName, user) {
+    const key = value[keyName];
+    const Value = _.omit(value, keyName);
+    const item = { Value };
+    const newItem = metadata.addMetadata({ resource: this.resource, key, item, accountName, user });
+    return createValue(newItem);
+  }
+
+  /**
+   * Create a resource in a Dynamo table that includes a sort key
+   */
+  createWithSortKey(value, partitionKey, sortKey, user) {
+    const item = { Value: value };
+    const newItem = metadata.addMetadata({ key: partitionKey, range: sortKey, resource: this.resource, item, accountName, user });
+    return createValue(newItem);
+  }
+
+  /**
+   * Update (replace) a single Dynamo resource
+   */
+  update(key, keyName, value, expectedVersion, user) {
+    const item = { Value:_.omit(value, keyName) };
+    const updatedItem = metadata.addMetadata({ resource: this.resource, key, item, expectedVersion, accountName, user });
+    return updateValue(updatedItem);
+  }
+
+  /**
+   * Update (replace) a single Dynamo resource in a table that incldudes a sort key
+   */
+  updateWithSortKey(value, partitionKey, sortKey, expectedVersion, user) {
+    const item = { Value:value };
+    const updatedItem = metadata.addMetadata({ key: partitionKey, range: sortKey, resource: this.resource, item, expectedVersion, accountName, user });
+    return updateValue(updatedItem);
+  }
+
+  /**
+   * Delete a single item from a Dynamo table
+   */
+  deleteItem(key, user) {
+    const deletedItem = metadata.addMetadata({ resource: this.resource, key, accountName, user });
+    return deleteValue(deletedItem);
+  }
+
+  /**
+   * Delete a single item from a Dynamo table that includes a sort key
+   */
+  deleteItemWithSortKey(partitionKey, sortKey, user) {
+    const deletedItem = metadata.addMetadata({ resource: this.resource, key: partitionKey, range: sortKey, accountName, user });
+    return deleteValue(deletedItem);
+  }
+
 }
 
-/**
- * Get a specific resource from a Dynamo table
- */
-function getByKey(resource, key) {
-  return getValue({ resource, key, exposeAudit, accountName });
-}
+module.exports = DynamoHelper;
 
-/**
- * Get a specific resource from a Dynamo table that has includes sort key
- */
-function getBySortKey(resource, partitionKey, sortKey) {
-  return getValue({ key: partitionKey, range: sortKey, resource, exposeAudit, accountName });
-}
-
-/**
- * Create a resource in a Dynamo table
- */
-function create(resource, value, keyName, user) {
-  const key = value[keyName];
-  const Value = _.omit(value, keyName);
-  const item = { Value };
-  const newItem = metadata.addMetadata({ resource, key, item, accountName, user });
-  return createValue(newItem);
-}
-
-/**
- * Create a resource in a Dynamo table that includes a sort key
- */
-function createWithSortKey(resource, value, partitionKey, sortKey, user) {
-  const item = { Value: value };
-  const newItem = metadata.addMetadata({ key: partitionKey, range: sortKey, resource, item, accountName, user });
-  return createValue(newItem);
-}
-
-/**
- * Update (replace) a single Dynamo resource
- */
-function update(resource, key, keyName, value, expectedVersion, user) {
-  const item = { Value:_.omit(value, keyName) };
-  const updatedItem = metadata.addMetadata({ resource, key, item, expectedVersion, accountName, user });
-  return updateValue(updatedItem);
-}
-
-/**
- * Update (replace) a single Dynamo resource in a table that incldudes a sort key
- */
-function updateWithSortKey(resource, value, partitionKey, sortKey, expectedVersion, user) {
-  const item = { Value:value };
-  const updatedItem = metadata.addMetadata({ key: partitionKey, range: sortKey, resource, item, expectedVersion, accountName, user });
-  return updateValue(updatedItem);
-}
-
-/**
- * Delete a single item from a Dynamo table
- */
-function deleteItem(resource, key, user) {
-  const deletedItem = metadata.addMetadata({ resource, key, accountName, user });
-  return deleteValue(deletedItem);
-}
-
-/**
- * Delete a single item from a Dynamo table that includes a sort key
- */
-function deleteItemWithSortKey(resource, partitionKey, sortKey, user) {
-  const deletedItem = metadata.addMetadata({ resource, key: partitionKey, range: sortKey, accountName, user });
-  return deleteValue(deletedItem);
-}
-
-module.exports = {
-  getAll,
-  getByKey,
-  getBySortKey,
-  create,
-  createWithSortKey,
-  update,
-  updateWithSortKey,
-  deleteItem,
-  deleteItemWithSortKey
-};
