57d34c5b03fb2eb1d6b89643472c94a330cf4c1d
Filip Sobczak <Filip.Sobczak@thetrainline.com>
2016-10-06 12:21:01 +0100

    Scan images for v1 API

--- server/api/controllers/images/imagesController.js
+++ server/api/controllers/images/imagesController.js
@@ -1,8 +1,27 @@
 /* Copyright (c) Trainline Limited, 2016. All rights reserved. See LICENSE.txt in the project root for license information. */
 'use strict';
 
-function getImages(req, res) {
-  res.json([{}, {}, {}]);
+let sender = require('modules/sender');
+
+function getImages(req, res, next) {
+  const accountName = req.swagger.params.account.value;
+  let query;
+
+  if (accountName === undefined) {
+    query = {
+      name: 'ScanCrossAccountImages',
+      filter: {},
+    };
+
+  } else {
+    query = {
+      name: 'ScanImages',
+      accountName,
+      filter: {},
+    };
+  }
+  
+  sender.sendQuery({ query }).then((data) => res.json(data)).catch(next);
 }
 
 module.exports = {
