912906517908d1cfd0db9097a0217ab0597cd3ff
Filip Sobczak <Filip.Sobczak@thetrainline.com>
2016-10-06 10:51:12 +0100

    Use authorizers for v1 API

--- server/api/v1.js
+++ server/api/v1.js
@@ -7,6 +7,7 @@ let swaggerTools = require('swagger-tools');
 let fs = require('fs');
 let defaultErrorHandler = require('./error-handler/defaultErrorHandler');
 let apiSpec = yaml.safeLoad(fs.readFileSync('api/swagger.yaml', 'utf8'));
+let authorization = require('modules/authorization');
 
 const API_BASE_PATH = apiSpec.basePath;
 const TOKEN_PATH = `${API_BASE_PATH}/token`;
@@ -17,13 +18,19 @@ let swaggerOptions = {
   ]
 };
 
-function simpleAuthorization(req, res, next) {
+function loggedInAuthorization(req, res, next) {
   if (req.user === undefined) {
     res.status(401);
     next({ message: 'Not authorized' })
   } else {
     next();
   }
+}
+
+function authorize(req, res, next) {
+  let authorizerName = req.swagger.operation['x-authorizer'] || 'simple';
+  let authorizer = require(`modules/authorizers/${authorizerName}`);
+  authorization(authorizer, req, res, next);
 }
 
 function permitTokenRequest(req, res, next) {
@@ -34,9 +41,10 @@ function permitTokenRequest(req, res, next) {
 function setup(app) {
   swaggerTools.initializeMiddleware(apiSpec, function (middleware) {
     app.use(TOKEN_PATH, permitTokenRequest);
-    app.use(API_BASE_PATH, simpleAuthorization);
+    app.use(API_BASE_PATH, loggedInAuthorization);
     app.use(middleware.swaggerMetadata());
     app.use(middleware.swaggerValidator());
+    app.use(API_BASE_PATH, authorize);
     app.use(middleware.swaggerRouter(swaggerOptions));
     app.use(middleware.swaggerUi());
     app.use(defaultErrorHandler);
