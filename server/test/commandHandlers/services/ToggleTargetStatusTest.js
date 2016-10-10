616b82d046b95a35ca3fa29bc02133dfb861b8d0
Duncan Hall <himself@duncanhall.net>
2016-10-10 12:41:02 +0100

    Additional Toggle Target State tests

--- server/test/commandHandlers/services/ToggleTargetStatusTest.js
+++ server/test/commandHandlers/services/ToggleTargetStatusTest.js
@@ -6,74 +6,76 @@ let assert = require('assert');
 let rewire = require('rewire');
 let sinon = require('sinon');
 
-describe('ToggleTargetStatus', function() {
+describe.only('ToggleTargetStatus', function() {
 
   const command = {
     environment: 'mock-environment',
     service: 'ReallyAwesomeService',
     serverRole: 'DoAllTheThings'
-  }
+  };
   
   const currentKeyValueState = {
     value: {}
   };
 
   let sut;
-  let serviceTargets = {
-    getTargetState: sinon.stub().returns(Promise.resolve(currentKeyValueState)),
-    setTargetState: sinon.stub().returns(Promise.resolve({}))
-  };
+  let serviceTargets;
 
   beforeEach(() => {
+    serviceTargets = {
+      getTargetState: sinon.stub().returns(Promise.resolve(currentKeyValueState)),
+      setTargetState: sinon.stub().returns(Promise.resolve({}))
+    };
+
     sut = rewire('commands/services/ToggleTargetStatus');
     sut.__set__({ serviceTargets });
   });
 
-  function expectServiceDisabled() {
-    return sut(command).then(result => assert.equal(result.Status, 'Disabled'));
+  function testEnablingAndDisabling() {
+    describe('requests to disable the service', () => {
+      beforeEach(() => command.enable = false);
+      it('should return a disabled service',
+        () => sut(command).then(result => assert.equal(result.Status, 'Disabled')));
+    });
+
+    describe('requests to enable the service', () => {
+      beforeEach(() => command.enable = true);
+      it('should return an enabled service',
+        () => sut(command).then(result => assert.equal(result.Status, 'Enabled')));
+    });
   }
 
-  function expectServiceEnabled() {
-    return sut(command).then(result => assert.equal(result.Status, 'Enabled'));
-  }
-
-  it('Constructs the expected key', () => {
+  it('uses the expected key format', () => {
    return sut(command).then(result => {
       let getStateCfg = serviceTargets.getTargetState.firstCall.args[1];
-      assert.equal(getStateCfg.key, `environments/${command.environment}/roles/${command.serverRole}/services/${command.service}/${command.slice}`);
+      assert.equal(getStateCfg.key,
+        `environments/${command.environment}/roles/${command.serverRole}/services/${command.service}/${command.slice}`);
     })
   });
 
-  describe('services with no Status defined', () => {
-
-    describe('requests to disable the service', () => {
-      beforeEach(() => command.enable = false)
-
-      it('should return a disabled service', expectServiceDisabled);
-    });
-
-    describe('requests to enable the service', () => {
-      beforeEach(() => command.enable = true)
-
-      it('should return an enabled service', expectServiceEnabled);
-    });
+  describe('on services with no Status defined', () => {
+    testEnablingAndDisabling();
   });
 
-
-  describe('services with an Enabled status', () => {
+  describe('on services with an Enabled status', () => {
     currentKeyValueState.value.Status = 'Enabled';
+    testEnablingAndDisabling();
+  });
 
-    describe('requests to disable the service', () => {
-      beforeEach(() => command.enable = false)
+  describe('on services with an Disabled status', () => {
+    currentKeyValueState.value.Status = 'Disabled';
+    testEnablingAndDisabling();
+  });
 
-      it('should return a disabled service', expectServiceDisabled);
-    });
+  describe('when a target state update errors', () => {
+    currentKeyValueState.value.Status = 'Enabled';
+    beforeEach(() => serviceTargets.setTargetState = sinon.stub().throws());
 
-    describe('requests to enable the service', () => {
-      beforeEach(() => command.enable = true)
-
-      it('should return an enabled service', expectServiceEnabled);
-    });
+    it('should provide a contextual error message', () => {
+      return sut(command).catch(error => {
+        return assert.equal(error.message,
+          `There was a problem updating the Future Deployment status for ${command.service}. Its status is still currently set to ${currentKeyValueState.value.Status}`)
+      })
+    })
   });
 });
-
