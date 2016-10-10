a838af0693d5e5c991bd40b4186e3357f4d5b391
Filip Sobczak <Filip.Sobczak@thetrainline.com>
2016-10-10 10:12:41 +0100

    Misc formatting

--- server/resources/deployments/DeploymentsCompletedDynamoResource.js
+++ server/resources/deployments/DeploymentsCompletedDynamoResource.js
@@ -1,3 +1,6 @@
+/* Copyright (c) Trainline Limited, 2016. All rights reserved. See LICENSE.txt in the project root for license information. */
+'use strict';
+
 module.exports = {
     name: 'deployments/completed',
     type: 'dynamodb/table',
