2251df88e2528bb0cf85fe6675b3f9508d192cca
Merlin Taylor <code.merlin.taylor@gmail.com>
2016-10-12 09:12:49 +0100

    Define package upload API

---
+++ server/api/controllers/package-upload-url/packageUploadUrlController.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+function getPackageUploadUrlByServiceVersion(id, version) {
+
+}
+
+function getPackageUploadUrlByServiceVersionEnvironment(id, version, environment) {
+
+}
+
+module.exports = {
+  getPackageUploadUrlByServiceVersion,
+  getPackageUploadUrlByServiceVersionEnvironment,
+};
