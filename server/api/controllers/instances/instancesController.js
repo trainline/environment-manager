c692ec684f5c929abe9a3b503b2929e3c83bb07a
Filip Sobczak <Filip.Sobczak@thetrainline.com>
2016-10-12 15:31:30 +0100

    Return instances v1 API

--- server/api/controllers/instances/instancesController.js
+++ server/api/controllers/instances/instancesController.js
@@ -1,8 +1,40 @@
 /* Copyright (c) Trainline Limited, 2016. All rights reserved. See LICENSE.txt in the project root for license information. */
 'use strict';
 
-function getInstances(req, res) {
-  res.json([{},{},{}]);
+let _ = require('lodash');
+let co = require('co');
+let ScanInstances = require('queryHandlers/ScanInstances');
+let ScanCrossAccountInstances = require('queryHandlers/ScanCrossAccountInstances');
+/**
+ * GET /instances
+ */
+function getInstances(req, res, next) {
+  const accountName = req.swagger.params.account.value;
+  const cluster = req.swagger.params.cluster.value;
+  const environment = req.swagger.params.environment.value;
+  const maintenance = req.swagger.params.maintenance.value;
+
+  co(function* () {
+    let filter = {};
+
+    if (cluster !== undefined) {
+      filter['tag:OwningCluster'] = cluster;
+    }
+    if (environment !== undefined) {
+      filter['tag:Environment'] = environment;
+    }
+    if (maintenance === true) {
+      asgIpsDynamo.getByKey('MAINTENANCE_MODE');
+      filter['private-ip-address']
+    }
+
+    if (_.isEmpty(filter)) {
+      filter = null;
+    }
+
+    let handler = accountName !== undefined ? ScanInstances : ScanCrossAccountInstances;
+    handler({ accountName, filter }).then((data) => res.json(data))
+  }).catch(next);
 }
 
 function getInstanceById(req, res) {
