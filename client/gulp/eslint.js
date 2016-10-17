78a2cd4b7e3d51eb8e65e705f7f447e04ea1f164
Duncan Hall <himself@duncanhall.net>
2016-10-17 15:50:17 +0100

    Implement airbnb ES5 lint without React dependencies

--- client/gulp/eslint.js
+++ client/gulp/eslint.js
@@ -3,7 +3,7 @@ var eslint = require('gulp-eslint');
 
 gulp.task('lint', function () {
 
-  return gulp.src(['**/*.js', '!node_modules/**'])
+  return gulp.src(['./app/**/*.js', '!node_modules/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
