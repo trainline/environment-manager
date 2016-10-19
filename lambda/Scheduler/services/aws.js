31e13c26988664400f636e4636cbd3f5f4c87bf3
davidihunt <david.hunt@gmx.co.uk>
2016-10-19 15:08:41 +0100

    Added dry runs for safety

--- lambda/Scheduler/services/aws.js
+++ lambda/Scheduler/services/aws.js
@@ -7,18 +7,19 @@ function createAWSService(config) {
   let ec2 = new AWS.EC2(config);
 
   function switchInstancesOn(instances) {
-    return ec2.startInstances({ InstanceIds: instances }).promise();
+    return ec2.startInstances({ InstanceIds: instances, DryRun: true }).promise();
   }
 
   function switchInstancesOff(instances) {
-    return ec2.stopInstances({ InstanceIds: instances }).promise();
+    return ec2.stopInstances({ InstanceIds: instances, DryRun: true }).promise();
   }
 
   function putAsgInstancesInService(asgs) {
     return Promise.all(asgs.map(asg => {
       return ec2.exitStandby({
         AutoScalingGroupName: asg.AutoScalingGroupName,
-        InstanceIds: asg.Instances 
+        InstanceIds: asg.Instances ,
+        DryRun: true
       }).promise();
     }));
   }
@@ -28,7 +29,8 @@ function createAWSService(config) {
       return ec2.enterStandby({
         AutoScalingGroupName: asg.AutoScalingGroupName,
         InstanceIds: asg.Instances,
-        ShouldDecrementDesiredCapacity: true
+        ShouldDecrementDesiredCapacity: true,
+        DryRun: true
       }).promise();
     }));
   }
