633060a1680b95597b668918d063e78b872ef8ba
Filip Sobczak <filsob@gmail.com>
2016-10-19 15:03:31 +0100

    Most basic healthcheck end point

--- server/api/controllers/diagnostics/diagnosticsController.js
+++ server/api/controllers/diagnostics/diagnosticsController.js
@@ -1,8 +1,9 @@
 /* Copyright (c) Trainline Limited, 2016. All rights reserved. See LICENSE.txt in the project root for license information. */
 'use strict';
 
-function getHealthcheck(req, res) {
-  res.json({});
+function getHealthcheck(req, res, next) {
+  // TODO(filip): implement some sanity checks - Redis, dynamodb, consul
+  res.json({ok: true}).catch(next);
 }
 
 module.exports = {
