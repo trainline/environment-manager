60298edc3f8b00858a0fc2ffeb699c433a7f6514
davidihunt <david.hunt@gmx.co.uk>
2016-11-17 16:04:34 +0000

    Added prompt to automatically scale out instances for refresh after launch config change

--- client/app/environments/dialogs/ASGDetailsModalController.js
+++ client/app/environments/dialogs/ASGDetailsModalController.js
@@ -208,8 +208,7 @@ angular.module('EnvironmentManager.environments').controller('ASGDetailsModalCon
       updated.SecurityGroups = vm.target.ASG.LaunchConfig.UI_SecurityGroupsFlatList.split(',').map(_.trim);
       delete updated.UI_SecurityGroupsFlatList;
       vm.asg.updateLaunchConfig(updated).then(function() {
-        showLaunchConfigConfirmation();
-        vm.refresh();
+        showLaunchConfigConfirmation().then(vm.refresh);
       }, function (error) {
         $rootScope.$broadcast('error', error);
       });
@@ -243,12 +242,10 @@ angular.module('EnvironmentManager.environments').controller('ASGDetailsModalCon
       var desired = vm.asgUpdate.DesiredCapacity;
       var max = vm.asgUpdate.MaxSize;
 
-      AutoScalingGroup.resize(vm.environmentName, vm.asg.AsgName, { min: min, desired: desired, max: max }).then(function () {
-        modal.information({
+      return AutoScalingGroup.resize(vm.environmentName, vm.asg.AsgName, { min: min, desired: desired, max: max }).then(function () {
+        return modal.information({
           title: 'ASG Resized',
           message: 'ASG resize successful. You can monitor instance changes by using the Refresh Icon in the top right of the window.<br/><br/><b>Note:</b> During scale-down instances will wait in a Terminating state for 10 minutes to allow for connection draining before termination.',
-        }).then(function () {
-          vm.refresh();
         });
       });
     };
@@ -258,9 +255,21 @@ angular.module('EnvironmentManager.environments').controller('ASGDetailsModalCon
     };
 
     function showLaunchConfigConfirmation() {
-      modal.information({
-        title: 'ASG Launch Configuration Updated',
-        message: 'Launch Configuration updated successfully.<br/><br/><b>PLEASE NOTE:</b> This change does not affect any existing instances. To rollout the change to existing instances:<ul><li>Resize the ASG to double its current size</li><li>Wait for the new instances to be brought into service</li><li>Resize the ASG to its original size again. This will remove older instances first leaving only those with the latest settings',
+      var instance = $uibModal.open({
+        templateUrl: '/app/environments/dialogs/asg/launchConfigConfirmation.html',
+        controller: 'LaunchConfigConfirmationController as vm',
+        resolve: {
+          parameters: function () { return { asg: vm.asg }; },
+        },
+      });
+      return instance.result.then(function(result){
+        if (result.doScalingRefresh) {
+          vm.asgUpdate.DesiredCapacity = result.numInstancesForRefresh;
+          if (vm.asgUpdate.MaxSize < result.numInstancesForRefresh) {
+            vm.asgUpdate.MaxSize = result.numInstancesForRefresh
+          }
+          return vm.resize();
+        }
       });
     }
     
