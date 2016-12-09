d7761a4e515560272b0cc8a665eafd5d933f484a
davidihunt <david.hunt@gmx.co.uk>
2016-12-09 10:09:56 +0000

    Deployments authorizer for patch operations

---
+++ server/modules/authorizers/deployments.js
@@ -0,0 +1,22 @@
+'use-strict'
+
+let co = require('co');
+let deploymentsHelper = require('modules/queryHandlersUtil/deployments-helper');
+
+module.exports = {
+  getRules: function (request) {
+    return co(function* () {
+
+      let key = request.swagger.params.id.value;
+      let deployment = yield deploymentsHelper.get({ key });
+
+      return [{
+        resource: request.url.replace(/\/+$/, ''),
+        access: request.method,
+        clusters: [deployment.Value.OwningCluster],
+        environmentTypes: [deployment.Value.EnvironmentType]
+      }];
+
+    });
+  }
+};
