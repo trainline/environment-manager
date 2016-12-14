4310a3d61131de2b7f69ea647860cac18ef989ed
Filip Sobczak <filsob@gmail.com>
2016-12-14 18:23:08 +0000

    Support promises

--- server/modules/S3GetObjectRequest.js
+++ server/modules/S3GetObjectRequest.js
@@ -1,7 +1,7 @@
 /* Copyright (c) Trainline Limited, 2016. All rights reserved. See LICENSE.txt in the project root for license information. */
 'use strict';
 
-var assert = require('assert');
+let assert = require('assert');
 
 function S3GetObjectRequest(client, parameters) {
   assert(client, `Invalid argument 'client'.`);
@@ -9,22 +9,27 @@ function S3GetObjectRequest(client, parameters) {
   assert(parameters.bucketName, `Invalid argument 'parameters.bucketName'.`);
   assert(parameters.objectPath, `Invalid argument 'parameters.objectPath'.`);
 
-  var self = this;
+  let self = this;
 
   self.execute = function (callback) {
-    var request = {
+    let request = {
       Bucket: parameters.bucketName,
       Key: parameters.objectPath,
     };
 
-    client.getObject(request, (error, data) => {
-      if (error) {
-        var message = `An error has occurred retrieving '${request.Key}' file from '${request.Bucket}' S3 bucket: ${error.message}`;
-        callback(new Error(message));
-      } else {
-        callback(null, data);
-      }
-    });
+    let promise = client.getObject(request)
+      .promise().then((data) => {
+        return data;
+      }, (error) => {
+        let message = `An error has occurred retrieving '${request.Key}' file from '${request.Bucket}' S3 bucket: ${error.message}`;
+        throw new Error(message);
+      });
+
+    if (callback !== undefined) {
+      promise.then((result) => callback(null, result), (error) => callback(error));
+    } else {
+      return promise;
+    }
   };
 }
 
