af2c46882e7e1844c41b6c7d55071dd165d6dee0
Filip Sobczak <filsob@gmail.com>
2016-12-14 17:47:40 +0000

    Extract component, DRY code

---
+++ client/app/environments/directives/serviceDiffWithTargetState.js
@@ -0,0 +1,12 @@
+/* Copyright (c) Trainline Limited, 2016. All rights reserved. See LICENSE.txt in the project root for license information. */
+'use strict';
+
+angular.module('EnvironmentManager.environments').component('serviceDiffWithTargetState', {
+  templateUrl: '/app/environments/directives/serviceDiffWithTargetState.html',
+  bindings: {
+    state: '<',
+  },
+  controllerAs: 'vm',
+  controller: function () {
+  }
+});
