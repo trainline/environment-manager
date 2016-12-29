fc8c562bbee5e0a021bd76e70a20e7300d231dba
Filip Sobczak <filsob@gmail.com>
2016-12-29 16:48:09 +0000

    Retain username on error

--- server/routes/form.js
+++ server/routes/form.js
@@ -17,11 +17,12 @@ const PUBLIC_DIR = config.get('PUBLIC_DIR');
 
 renderer.register('login', `${PUBLIC_DIR}/login.html`);
 
-function serveLoginPage(response, error) {
+function serveLoginPage(response, error, username) {
   let content = {
     data: {
       error: error ? error.message : undefined,
       version: APP_VERSION,
+      username: username
     },
   };
 
@@ -64,7 +65,7 @@ module.exports = {
         response.redirect(targetUrl);
       }).catch((error) => {
         logger.warn(error);
-        serveLoginPage(response, error);
+        serveLoginPage(response, error, request.body.username);
       });
     },
   },
