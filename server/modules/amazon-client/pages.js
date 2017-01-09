2d8a688b76a5a34fbf646d344dcf355bdb4e857b
Merlin Taylor <code.merlin.taylor@gmail.com>
2017-01-09 16:38:18 +0000

    reduce over pages of  AWSRequest

---
+++ server/modules/amazon-client/pages.js
@@ -0,0 +1,65 @@
+/* Copyright (c) Trainline Limited, 2016-2017. All rights reserved. See LICENSE.txt in the project root for license information. */
+
+'use strict';
+
+/**
+ * Analogue of Array.reduce that iterates over the pages of an AWS Request object.
+ *
+ * @param {function} callback - (accumulator, page) -> accumulator.
+ * @param {any} initialValue - initial value of accumulator.
+ * @param {object} awsRequest - an instance of AWS.Request.
+ *   See http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Request.html#eachPage
+ * @returns {any} - returns the value of accumulator after applying callback to the
+ *   last page.
+ */
+function reduce(callback, initialValue, awsRequest) {
+  if (typeof callback !== 'function') {
+    throw new Error('callback must be a function');
+  }
+
+  let accumulator = initialValue;
+  return new Promise((resolve, reject) => {
+    awsRequest.eachPage((error, data) => {
+      if (error) {
+        reject(error);
+      } else if (data === null) {
+        resolve(accumulator);
+      } else {
+        accumulator = callback(accumulator, data);
+      }
+    });
+  });
+}
+
+/**
+ * Flattens the result pages of an AWS Request object into a single array.
+ *
+ * @param {function} callback - page -> items. Selects the items that should be
+ *   returned from the page.
+ * @param {object} awsRequest - an instance of AWS.Request.
+ *   See http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Request.html#eachPage
+ * @returns {array} - returns the value of accumulator after applying callback to the
+ *   last page.
+ */
+function flatten(callback, awsRequest) {
+  if (typeof callback !== 'function') {
+    throw new Error('callback must be a function');
+  }
+
+  function extractAndConcatItems(accumulator, page) {
+    let items = callback(page);
+    if (Array.isArray(items)) {
+      items.forEach(item => accumulator.push(item));
+    } else {
+      accumulator.push(items);
+    }
+    return accumulator;
+  }
+
+  return reduce(extractAndConcatItems, [], awsRequest);
+}
+
+module.exports = {
+  reduce,
+  flatten,
+};
---
+++ server/test/modules/amazon-client/pagesTest.js
@@ -0,0 +1,71 @@
+/* Copyright (c) Trainline Limited, 2016-2017. All rights reserved. See LICENSE.txt in the project root for license information. */
+
+/* eslint func-names: 0, import/no-extraneous-dependencies: 0, prefer-arrow-callback: 0 */
+
+'use strict';
+
+const should = require('should');
+const pages = require('modules/amazon-client/pages');
+
+let flatten = pages.flatten;
+
+describe('pages', function () {
+    describe('flatten', function () {
+        context('when AWS api returns an error', function () {
+            it('it returns a rejected promise', function () {
+                let awsRequest = {
+                    eachPage: (cb) => {
+                        cb(null, 1);
+                        cb(new Error('some error'));
+                    }
+                }
+                return flatten(x => x, awsRequest).should.be.rejected(/some error/);
+            })
+        });
+        context('when AWS api returns no pages', function () {
+            it('it returns a promise of an empty array', function () {
+                let awsRequest = {
+                    eachPage: (cb) => {
+                        cb(null, null);
+                    }
+                }
+                return flatten(x => x, awsRequest).should.finally.eql([]);
+            })
+        });
+        context('when AWS api returns an empty page', function () {
+            it('it returns a promise of an empty array', function () {
+                let awsRequest = {
+                    eachPage: (cb) => {
+                        cb(null, []);
+                        cb(null, null);
+                    }
+                }
+                return flatten(x => x, awsRequest).should.finally.eql([]);
+            })
+        });
+        context('when AWS api returns a sequence of single-item pages', function () {
+            it('it returns a promise of a flat array of the items', function () {
+                let awsRequest = {
+                    eachPage: (cb) => {
+                        cb(null, 1);
+                        cb(null, 2);
+                        cb(null, null);
+                    }
+                }
+                return flatten(x => x, awsRequest).should.finally.eql([1, 2]);
+            })
+        });
+        context('when AWS api returns a sequence of multi-item pages', function () {
+            it('it returns a promise of a flat array of the items', function () {
+                let awsRequest = {
+                    eachPage: (cb) => {
+                        cb(null, [1, 2]);
+                        cb(null, [3, 4]);
+                        cb(null, null);
+                    }
+                }
+                return flatten(x => x, awsRequest).should.finally.eql([1, 2, 3, 4]);
+            })
+        });
+    });
+});
