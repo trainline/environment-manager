4f6438b21b790d17b205904840bca6c2a2c9a9f4
Duncan Hall <himself@duncanhall.net>
2017-01-10 10:09:09 +0000

    Remove BOM

--- server/queryHandlers/ScanServersStatus.js
+++ server/queryHandlers/ScanServersStatus.js
@@ -1,4 +1,4 @@
-﻿/* Copyright (c) Trainline Limited, 2016. All rights reserved. See LICENSE.txt in the project root for license information. */
+/* Copyright (c) Trainline Limited, 2016. All rights reserved. See LICENSE.txt in the project root for license information. */
 
 'use strict';
 
