2357417899c84a18c9f559da25c654fcf86f2e84
Filip Sobczak <filsob@gmail.com>
2017-01-11 16:50:40 +0000

    Single quote

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     "angular/di": ['error', 'array'],
     "indent": ['error', 2],
     "linebreak-style": ['off'],
-    "quotes": ['error', 'double', { avoidEscape: false, allowTemplateLiterals: true }],
+    "quotes": ['error', 'single', { avoidEscape: false, allowTemplateLiterals: true }],
     "semi": ['error', 'always'],
     "no-var": 'off', // We don't use ES6
     "no-param-reassign": ['error', { props: false }],
