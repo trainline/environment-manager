257c64eb7edbdae891f600a8775a16b5a2af760e
Merlin Taylor <code.merlin.taylor@gmail.com>
2017-01-11 13:23:06 +0000

    Add circuit breaker to Redis cache

--- server/modules/data-access/cacheManagerEncryptedRedis.js
+++ server/modules/data-access/cacheManagerEncryptedRedis.js
@@ -2,67 +2,103 @@
 
 let fp = require('lodash/fp');
 let logger = require('modules/logger');
+let memoize = require('modules/memoize');
 let Redis = require('ioredis');
+let timers = require('timers');
 
-function connectToRedis({ address, port }) {
-  return new Promise((resolve, reject) => {
-    let client = new Redis(address, port);
-    let events = ['close', 'connect', 'end', 'error', 'ready', 'reconnecting'];
-    events.forEach((name) => {
-      client.on(name, (e) => {
-        logger.debug(`${name}: redis ${address}:${port}`);
-        if (e) {
-          logger.debug(e);
-        }
-      });
-    });
-    client.once('ready', () => resolve(client));
-    client.on('error', e => logger.error(e));
-  });
+const NOT_FOUND = undefined;
+
+function delay(milliseconds) {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-function usingRedisConnection(connectionFactory, operation) {
-  return Promise.resolve(connectionFactory())
-    .then(redis => Promise.resolve(operation(redis)).then(
-      result => redis.quit().then(() => result),
-      e => redis.quit().then(() => Promise.reject(e))));
+function timeout(t) {
+  return delay(t).then(() => Promise.reject(new Error(`operation timed out after ${t} milliseconds`)));
+}
+
+function logError(error) {
+  logger.error(error);
+  return NOT_FOUND;
+}
+
+function circuitBreaker(policy) {
+  let state = 'CLOSED';
+  let failures = 0;
+  function open() {
+    state = 'OPEN';
+    timers.setTimeout(() => { state = 'HALF OPEN'; }, policy.resetTimeout);
+  }
+  function close() {
+    state = 'CLOSED';
+  }
+  return (fn) => {
+    if (state === 'OPEN') {
+      return Promise.reject(new Error('Circuit Breaker open.'));
+    } else if (state === 'HALF OPEN') {
+      return Promise.resolve(fn()).then((x) => { close(); return x; }, (e) => { open(); return Promise.reject(e); });
+    } else {
+      return Promise.resolve(fn()).catch((e) => {
+        failures += 1;
+        timers.setTimeout(() => { failures -= 1; }, policy.failureTimeout);
+        if (failures > policy.maxFailures) {
+          open();
+        }
+        return Promise.reject(e);
+      });
+    }
+  };
+}
+
+function connectToRedis({ address, port }) {
+  let client = new Redis({ host: address, port, lazyConnect: true, connectTimeout: 1000 });
+  let events = ['close', 'connect', 'end', 'error', 'ready', 'reconnecting'];
+  events.forEach((name) => {
+    client.on(name, (e) => {
+      logger.debug(`${name}: redis ${address}:${port}`);
+      if (e) {
+        logger.debug(e);
+      }
+    });
+  });
+  client.on('error', e => logger.error(e));
+  return client;
 }
 
 function encryptedRedisStore(args) {
-  let connect = connectToRedis.bind(null, { address: args.host, port: args.port });
+  const WRITE_TIMEOUT = args.writeTimeout || 100;
+  const READ_TIMEOUT = args.readTimeout || 100;
+
+  let redis = connectToRedis({ address: args.host, port: args.port });
+  let breaker = circuitBreaker({ failureTimeout: 1000, maxFailures: 2, resetTimeout: 1000 });
   let id = x => x;
   let keyToStore = fp.get(['keyTransform', 'toStore'])(args) || id;
   let keyFromStore = fp.get(['keyTransform', 'fromStore'])(args) || id;
   let valueToStore = fp.get(['valueTransform', 'toStore'])(args) || fp.flow(JSON.stringify, str => new Buffer(str));
   let valueFromStore = fp.get(['valueTransform', 'fromStore'])(args) || fp.flow(buf => buf.toString(), JSON.parse);
 
-  function del(key, options, cb) {
-    if (typeof options === 'function') {
-      cb = options;
-    }
+  function del(key, options, callback) {
+    let cb = (typeof options === 'function') ? options : callback;
 
     let skey = keyToStore(key);
-    let promise = usingRedisConnection(connect, redis => redis.del(skey));
+    let promise = breaker(() => Promise.race([timeout(WRITE_TIMEOUT), redis.del(skey)])).catch(logError);
 
     if (cb) {
-      promise.then(cb.bind(null, null), cb.bind(null));
+      return promise.then(cb.bind(null, null), cb.bind(null));
     } else {
       return promise;
     }
   }
 
-  function get(key, options, cb) {
-    if (typeof options === 'function') {
-      cb = options;
-    }
+  function get(key, options, callback) {
+    let cb = (typeof options === 'function') ? options : callback;
 
     let skey = keyToStore(key);
 
-    let promise = usingRedisConnection(connect, redis => redis.getBuffer(skey))
-      .then(value => (value ? valueFromStore(value) : undefined));
+    let promise = breaker(() => Promise.race([timeout(READ_TIMEOUT), redis.getBuffer(skey)]))
+      .then(value => (value ? valueFromStore(value) : NOT_FOUND), logError);
 
     if (cb) {
-      promise.then(cb.bind(null, null), cb.bind(null));
+      return promise.then(cb.bind(null, null), cb.bind(null));
     } else {
       return promise;
     }
@@ -71,44 +107,41 @@ function encryptedRedisStore(args) {
   function keys(cb) {
     let error = new Error('Encrypted Redis store does not support the "keys" operation');
     if (cb) {
-      cb(error);
+      return cb(error);
     } else {
       return Promise.reject(error);
     }
   }
 
   function reset(cb) {
-    let promise = usingRedisConnection(connect, redis => redis.flushdb());
+    let promise = breaker(() => Promise.race([timeout(WRITE_TIMEOUT), redis.flushdb()])).catch(logError);
 
     if (cb) {
-      promise.then(cb.bind(null, null), cb.bind(null));
+      return promise.then(cb.bind(null, null), cb.bind(null));
     } else {
       return promise;
     }
   }
 
-  function set(key, value, options, cb) {
-    if (typeof options === 'function') {
-      cb = options;
-      options = {};
-    }
-    options = options || {};
+  function set(key, value, options, callback) {
+    let cb = (typeof options === 'function') ? options : callback;
+    let opts = (typeof options === 'function') ? {} : (options || {});
 
     let skey = keyToStore(key);
     let svalue = valueToStore(value);
     let redisOperation = (() => {
-      let ttl = fp.get('ttl')(options);
+      let ttl = fp.get('ttl')(opts);
       if (ttl) {
-        return redis => redis.setexBuffer(skey, ttl, svalue);
+        return () => redis.setexBuffer(skey, ttl, svalue);
       } else {
-        return redis => redis.setBuffer(skey, svalue);
+        return () => redis.setBuffer(skey, svalue);
       }
     })();
 
-    let promise = usingRedisConnection(connect, redisOperation);
+    let promise = breaker(() => Promise.race([timeout(WRITE_TIMEOUT), redisOperation()])).catch(logError);
 
     if (cb) {
-      promise.then(cb.bind(null, null), cb.bind(null));
+      return promise.then(cb.bind(null, null), cb.bind(null));
     } else {
       return promise;
     }
@@ -124,5 +157,5 @@ function encryptedRedisStore(args) {
 }
 
 module.exports = {
-  create: encryptedRedisStore,
+  create: memoize(encryptedRedisStore),
 };
