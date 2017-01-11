1096a965833f80281a50f42966a0487c1178deb5
Filip Sobczak <filsob@gmail.com>
2017-01-11 10:56:46 +0000

    Update copyright

--- server/modules/cacheManager.js
+++ server/modules/cacheManager.js
@@ -1,4 +1,4 @@
-/* Copyright (c) Trainline Limited, 2016. All rights reserved. See LICENSE.txt in the project root for license information. */
+/* Copyright (c) Trainline Limited, 2016-2017. All rights reserved. See LICENSE.txt in the project root for license information. */
 /**
  * An in-memory cache that decouples the getting of an item from
  * the handling of a cache miss.
