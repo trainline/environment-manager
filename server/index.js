09d6a1db81f3aa949fa2f50717c6815700ce6c45
Merlin Taylor <code.merlin.taylor@gmail.com>
2017-01-16 12:58:56 +0000

    Clear cache on application startup

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ let logger = require('modules/logger');
 // TODO conver to singleton
 let ConfigurationProvider = require('modules/configuration/ConfigurationProvider');
 let checkAppPrerequisites = require('modules/checkAppPrerequisites');
+let cacheManager = require('modules/cacheManager');
 let co = require('co');
 require('./globals');
 
@@ -40,6 +41,8 @@ function start() {
       new MainServer(),
       new AdminServer()
     ];
+
+    yield cacheManager.clear();
 
     yield servers.map(server => server.start());
   }).catch((error) => {
