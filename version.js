0647c3215ef0a7766a916fa0d06ca8d481948c11
Merlin Taylor <code.merlin.taylor@gmail.com>
2017-01-27 11:07:32 +0000

    Fix npm version

---
+++ version.js
@@ -0,0 +1,18 @@
+'use strict'
+
+let childProcess = require('child_process');
+let path = require('path');
+let process = require('process');
+
+let version = process.env['npm_package_version'];
+
+function versionPackage(rootDirectory) {
+    return childProcess.execSync(`npm version "${version}" && git add -A`, {
+        cwd: rootDirectory,
+        env: process.env
+    })
+}
+
+['client', 'server']
+    .map(relDir => path.resolve(process.cwd(), relDir))
+    .forEach(versionPackage);
