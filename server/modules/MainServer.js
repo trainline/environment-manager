d0272b2914435501b20faab31ba4996520234e35
Filip Sobczak <filsob@gmail.com>
2017-02-01 18:07:29 +0000

    Update file with develop changes

--- server/modules/MainServer.js
+++ server/modules/MainServer.js
@@ -1,37 +1,48 @@
-/* Copyright (c) Trainline Limited, 2016. All rights reserved. See LICENSE.txt in the project root for license information. */
+/* Copyright (c) Trainline Limited, 2016-2017. All rights reserved. See LICENSE.txt in the project root for license information. */
+
 'use strict';
 
 let express = require('express');
 let bodyParser = require('body-parser');
 let cookieParser = require('cookie-parser');
 let logger = require('modules/logger');
-let winston = require('winston');
-let fs = require('fs');
+let fp = require('lodash/fp');
 let config = require('config/');
 let compression = require('compression');
 let expressWinston = require('express-winston');
 
-let serverFactoryConfiguration = new(require('modules/serverFactoryConfiguration'))();
+let serverFactoryConfiguration = new (require('modules/serverFactoryConfiguration'))();
 let tokenAuthentication = require('modules/authentications/tokenAuthentication');
 let cookieAuthentication = require('modules/authentications/cookieAuthentication');
 let authentication = require('modules/authentication');
 let deploymentMonitorScheduler = require('modules/monitoring/DeploymentMonitorScheduler');
 let apiV1 = require('api/v1');
+let httpServerFactory = require('modules/http-server-factory');
+
+let serverInstance;
 
 const APP_VERSION = require('config').get('APP_VERSION');
 
-let httpServerFactory = require('modules/http-server-factory');
-let _server;
+function requestFilter(req, propName) {
+  // Avoid writing the authorization token to the log.
+  if (propName === 'headers') {
+    return fp.omit(['authorization'])(req[propName]);
+  }
+  return req[propName];
+}
+
+let expressWinstonOptions = {
+  requestFilter,
+  winstonInstance: logger
+};
 
 function createExpressApp() {
   return new Promise((resolve) => {
-
     let routeInstaller = require('modules/routeInstaller');
     let routes = {
       home: require('routes/home'),
-      form: require('routes/form'),
       initialData: require('routes/initialData'),
-      deploymentNodeLogs: require('routes/deploymentNodeLogs'),
+      deploymentNodeLogs: require('routes/deploymentNodeLogs')
     };
 
     // start express
@@ -47,7 +58,7 @@ function createExpressApp() {
     /* notice how the router goes after the logger.
      * https://www.npmjs.com/package/express-winston#request-logging */
     if (config.get('IS_PRODUCTION') === true) {
-      app.use(expressWinston.logger({ winstonInstance: logger }));
+      app.use(expressWinston.logger(expressWinstonOptions));
     }
 
     const PUBLIC_DIR = config.get('PUBLIC_DIR');
@@ -55,40 +66,35 @@ function createExpressApp() {
 
     let staticPaths = ['*.js', '*.css', '*.html', '*.ico', '*.gif', '*.woff2', '*.ttf', '*.woff', '*.svg', '*.eot', '*.jpg', '*.png', '*.map'];
     app.get(staticPaths, authentication.allowUnknown, express.static(PUBLIC_DIR));
-    app.get('/', authentication.denyUnauthorized, express.static(PUBLIC_DIR));
+    app.get('/', express.static(PUBLIC_DIR));
 
     app.get('*.js', authentication.allowUnknown, express.static('modules'));
 
     // routing for API JSON Schemas
     app.use('/schema', authentication.allowUnknown, express.static(`${PUBLIC_DIR}/schema`));
 
-    // routing for html pages
-    app.get('/login', authentication.allowUnknown, routes.form.login.get);
-    app.post('/login', authentication.allowUnknown, routes.form.login.post);
-    app.get('/logout', authentication.allowUnknown, routes.form.logout.get);
-
     app.get('/deployments/nodes/logs', authentication.denyUnauthorized, routes.deploymentNodeLogs);
 
     // routing for APIs
-    app.get('/api/initial-data', authentication.denyUnauthorized, routes.initialData);
+    app.get('/api/initial-data', routes.initialData);
     app.use('/api', routeInstaller());
 
     if (config.get('IS_PRODUCTION') === true) {
-      app.use(expressWinston.errorLogger({ winstonInstance: logger }));
+      app.use(expressWinston.errorLogger(expressWinstonOptions));
     }
 
     apiV1.setup(app);
 
     resolve(app);
   });
-};
+}
 
 function createServer(app) {
   let parameters = {
-    port: serverFactoryConfiguration.getPort(),
+    port: serverFactoryConfiguration.getPort()
   };
 
-  return httpServerFactory.create(app, parameters).then(server => {
+  return httpServerFactory.create(app, parameters).then((server) => {
     logger.info(`Main server created using ${httpServerFactory.constructor.name} service.`);
     logger.info(`Main server listening at port ${parameters.port}.`);
     return Promise.resolve(server);
@@ -96,7 +102,7 @@ function createServer(app) {
 }
 
 function initializeServer(server) {
-  _server = server;
+  serverInstance = server;
   deploymentMonitorScheduler.start();
   logger.info(`EnvironmentManager v.${APP_VERSION} started!`);
 }
@@ -108,7 +114,6 @@ module.exports = {
       .then(initializeServer);
   },
   stop: function () {
-    _server.close();
+    serverInstance.close();
   }
-}
-
+};
