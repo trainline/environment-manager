463187a06808d82000649c9c68fad9748cbfedd7
Jake Cross <jake.cross@thetrainline.com>
2017-02-21 14:36:38 +0000

    replaced calls in envronments route handler - aws

--- server/routes/api/ops/environmentsRouteHandlerDescriptor.js
+++ server/routes/api/ops/environmentsRouteHandlerDescriptor.js
@@ -5,21 +5,23 @@
 let send = require('modules/helpers/send');
 let route = require('modules/helpers/route');
 let authorizer = require('modules/authorizers/environments-schedule');
-let config = require('config');
+let awsAccounts = require('modules/awsAccounts');
 
 module.exports = route.put('/ops/environments/:key')
   .withDocs({ disableDocs: true })
   .withAuthorizer(authorizer)
   .do((request, response) => {
-    const masterAccountName = config.getUserValue('masterAccountName');
-    let command = {
-      name: 'UpdateDynamoResource',
-      resource: 'ops/environments',
-      key: request.params.key,
-      item: request.body,
-      expectedVersion: request.headers['expected-version'],
-      accountName: masterAccountName
-    };
+    awsAccounts.getMasterAccountName()
+      .then((masterAccountName) => {
+        let command = {
+          name: 'UpdateDynamoResource',
+          resource: 'ops/environments',
+          key: request.params.key,
+          item: request.body,
+          expectedVersion: request.headers['expected-version'],
+          accountName: masterAccountName
+        };
 
-    send.command(command, request, response);
+        send.command(command, request, response);
+      });
   });
