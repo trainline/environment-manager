0faab84267acac998a2d68292d65c04f4dd6b1ae
davidihunt <david.hunt@gmx.co.uk>
2017-03-03 17:54:53 +0000

    First stab at schedule viewer UI changes

--- client/app/common/directives/scheduleViewer.js
+++ client/app/common/directives/scheduleViewer.js
@@ -31,18 +31,45 @@ angular.module('EnvironmentManager.common')
 
           delete vm.crons;
         } else {
-          var parts = vm.schedule.split('|');
-          vm.crons = parts[0].split(';').map(function (cronString) {
-            if (cronString) {
-              var parts = cronString.split(':');
-              var action = parts[0].trim() + 's ';
-              var englishCron = prettyCron.toString(parts[1].trim());
-              return { cron: _.capitalize(action) + englishCron };
-            }
-          });
-          var timezone = (parts.length === 2) ? parts[1].trim() : 'UTC';
-          vm.timezone = 'Time Zone:' + timezone;
+          var schedule = parseScheduleTag(vm.schedule);
+
+          vm.timezone = schedule.timezone.readable;
+          vm.crons = schedule.schedules.map(s => ({ cron: s.readable }));
+
+          var next = _.minBy(schedule.schedules, s => s.next.format('YYYY-MM-DDTHH:mm:ss'));
+          vm.next = next.action + ': ' + next.next.format('ddd HH:mm') + ' (' + moment(next.next).tz('UTC').format('ddd HH:mm z') + ')';
         }
+      }
+
+      function parseScheduleTag(scheduleTag) {
+        let [cronsPart, timezonePart] = scheduleTag.split('|');
+        let timezoneCode = timezonePart ? timezonePart.trim() : 'UTC';
+
+        let currentLocalTime = moment.tz(moment.utc(), timezoneCode);
+        let timezone = timezoneCode + ' - Currently ' + currentLocalTime.format('HH:mm z');
+
+        let serialisedCrons = cronsPart.split(';');
+        let schedules = serialisedCrons.map((item) => {
+          let parts = item.split(':');
+
+          let action = _.capitalize(parts[0].trim());
+          let cron = parts[1].trim();
+
+          let englishSchedule = prettyCron.toString(cron);
+          let readable = action + ': ' + englishSchedule.replace('Mon, Tue, Wed, Thu and Fri', 'Weekdays');
+
+          let schedule = later.parse.cron(cron);
+          let nextOccurrence = later.schedule(schedule).next(1, currentLocalTime.format('YYYY-MM-DDTHH:mm:ss'));
+          let next = moment.tz(moment(nextOccurrence).format('YYYY-MM-DDTHH:mm:ss'), timezoneCode);
+
+          return { action, cron, readable, next };
+        });
+
+        return {
+          schedules,
+          timezone: { code: timezoneCode, readable: timezone },
+          currentLocalTime
+        };
       }
 
       $scope.$watch('vm.schedule', update);
