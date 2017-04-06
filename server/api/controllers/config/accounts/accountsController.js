09324a75590b0643a14c434547548c49062c5d07
Jake Cross <jake.cross@thetrainline.com>
2017-04-06 14:31:38 +0100

    Added publish methods to accountsController

--- server/api/controllers/config/accounts/accountsController.js
+++ server/api/controllers/config/accounts/accountsController.js
@@ -6,6 +6,7 @@ let getAccounts = require('queryHandlers/GetAWSaccounts');
 let addAccount = require('commands/aws/AddAWSaccount');
 let updateAccount = require('commands/aws/UpdateAWSaccount');
 let removeAccount = require('commands/aws/RemoveAWSaccount');
+let sns = require('modules/sns/EnvironmentManagerEvents');
 
 /**
  * GET /config/accounts
@@ -21,7 +22,20 @@ function postAccountsConfig(req, res, next) {
   const account = req.swagger.params.account.value;
   const user = req.user;
 
-  return addAccount({ account, user }).then(() => res.status(201).end()).catch(next);
+  return addAccount({
+      account,
+      user
+    })
+    .then(() => res.status(201).end())
+    .then(sns.publish({
+      message: 'POST /config/accounts',
+      topic: sns.TOPICS.CONFIGURATION_CHANGE,
+      attributes: {
+        Action: sns.ACTIONS.POST,
+        ID: account.AccountNumber
+      }
+    }))
+    .catch(next);
 }
 
 /**
@@ -33,7 +47,20 @@ function putAccountConfigByName(req, res, next) {
   const user = req.user;
   account.AccountNumber = accountNumber; // Prevent attempts to update the key
 
-  return updateAccount({ account, user }).then(() => res.status(200).end()).catch(next);
+  return updateAccount({
+      account,
+      user
+    })
+    .then(() => res.status(200).end())
+    .then(sns.publish({
+      message: `PUT /config/accounts/${accountNumber}`,
+      topic: sns.TOPICS.CONFIGURATION_CHANGE,
+      attributes: {
+        Action: sns.ACTIONS.PUT,
+        ID: accountNumber
+      }
+    }))
+    .catch(next);
 }
 
 /**
@@ -43,7 +70,20 @@ function deleteAccountConfigByName(req, res, next) {
   const accountNumber = req.swagger.params.accountNumber.value;
   const user = req.user;
 
-  return removeAccount({ accountNumber, user }).then(() => res.status(200).end()).catch(next);
+  return removeAccount({
+      accountNumber,
+      user
+    })
+    .then(() => res.status(200).end())
+    .then(sns.publish({
+      message: `Delete /config/accounts/${accountNumber}`,
+      topic: sns.TOPICS.CONFIGURATION_CHANGE,
+      attributes: {
+        Action: sns.ACTIONS.DELETE,
+        ID: accountNumber
+      }
+    }))
+    .catch(next);
 }
 
 module.exports = {
