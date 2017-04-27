887892981914c08be8fbbf8726b82e60e4e42056
Duncan Hall <himself@duncanhall.net>
2017-04-27 12:55:33 +0100

    Copy debug unit definition

--- server/deployment/code-deploy/on-after-install.sh
+++ server/deployment/code-deploy/on-after-install.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-SERVICE_DEFINITION=environment-manager.service
-SRC_FILE=/opt/environment-manager/deployment/systemd/${SERVICE_DEFINITION}
-TARGET_FILE=/lib/systemd/system/${SERVICE_DEFINITION}
+create_systemd_unit() {
+  local SERVICE_DEFINITION=$1
+  local SRC_FILE=/opt/environment-manager/deployment/systemd/${SERVICE_DEFINITION}
+  local TARGET_FILE=/lib/systemd/system/${SERVICE_DEFINITION}
+  
+  cp -f ${SRC_FILE} ${TARGET_FILE}
+  chmod 644 ${TARGET_FILE}
+  chown root.root ${TARGET_FILE}
+}
 
-cp -f ${SRC_FILE} ${TARGET_FILE}
-
-chmod 644 ${TARGET_FILE}
-
-chown root.root ${TARGET_FILE}
+create_systemd_unit "environment-manager.service"
+create_systemd_unit "environment-manager-debug.service"
