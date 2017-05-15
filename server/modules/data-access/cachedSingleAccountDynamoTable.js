88bf4889a711b50abcdaf95c2f69d80b40e78d65
Merlin Taylor <code.merlin.taylor@gmail.com>
2017-05-15 11:33:41 +0100

    PD-242 Correct casing of "singleAccountDynamoTable"

--- server/modules/data-access/cachedSingleAccountDynamoTable.js
+++ server/modules/data-access/cachedSingleAccountDynamoTable.js
@@ -2,12 +2,12 @@
 
 'use strict';
 
-let singleAccountdynamoTable = require('modules/data-access/singleAccountdynamoTable');
+let singleAccountDynamoTable = require('modules/data-access/singleAccountDynamoTable');
 let dynamoTableCache = require('modules/data-access/dynamoTableCache');
 
 function factory(physicalTableName, { ttl }) {
   let cachedTable = dynamoTableCache(physicalTableName, { ttl });
-  return singleAccountdynamoTable(physicalTableName, cachedTable);
+  return singleAccountDynamoTable(physicalTableName, cachedTable);
 }
 
 module.exports = factory;
