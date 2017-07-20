14e6c781aa8e0c1fb259323ffcb33b7b6ada2ea6
davidihunt <david.hunt@gmx.co.uk>
2017-07-20 16:09:43 +0100

    Added em install script to user-data

--- em2/deploy/em-master/scripts/app/user-data.sh
+++ em2/deploy/em-master/scripts/app/user-data.sh
@@ -9,3 +9,10 @@ aws s3api get-object --bucket em-testing-init --key em-asg-setup.sh ./em-asg-set
 #execute init script
 chmod 700 ./em-asg-setup.sh
 ./em-asg-setup.sh
+
+#fetch install script from s3
+aws s3api get-object --bucket em-testing-init --key em-install.sh ./em-install.sh
+
+#execute install script
+chmod 700 ./em-install.sh
+./em-install.sh
