46acdba7dd3eaa7f438b6c81df6278ac6c942942
davidihunt <david.hunt@gmx.co.uk>
2017-07-26 11:24:56 +0100

    Removed unnecessary file

--- server/queryHandlers/GetAWSHostAccount.js
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) Trainline Limited, 2016-2017. All rights reserved. See LICENSE.txt in the project root for license information. */
-
-'use strict';
-
-let myIdentity = require('modules/amazon-client/myIdentity');
-
-module.exports = () => {
-  return myIdentity().then(ident => ({ id: Number(ident.Account) }));
-};
