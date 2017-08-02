da013e7909dcf432bf4ae46a4969aebff4528a35
davidihunt <david.hunt@gmx.co.uk>
2017-08-02 10:28:14 +0100

    Fixed local scheduler execution

--- setup/cloudformation/lambda/InfraEnvironmentManagerScheduler/local/index.js
+++ setup/cloudformation/lambda/InfraEnvironmentManagerScheduler/local/index.js
@@ -6,10 +6,24 @@ let emFactory = require('../services/em');
 let schedulerFactory = require('../scheduler');
 
 let awsConfig = { region: 'eu-west-1' };
-let em = emFactory.create(config.em);
+
+function ConsoleLogger() {
+  let logFn = console.log;
+  this.error = logFn;
+  this.warn = logFn;
+  this.info = logFn;
+  this.log = logFn;
+  this.debug = logFn;
+  this.close = () => {};
+  this.createSubLogger = _ => this;
+}
+
+let logger = new ConsoleLogger();
+
+let em = emFactory.create(config.em, logger);
 
 let context = { awsAccountId: 123456789, awsRegion: 'eu-west-1', env: { CHILD_ACCOUNT_ROLE_NAME: 'enterRoleName' } };
-let scheduler = schedulerFactory.create(config, em, require('aws-sdk'), context);
+let scheduler = schedulerFactory.create(config, em, require('aws-sdk'), context, logger);
 
 let write = result => console.log(JSON.stringify(result, null, 2));
 
