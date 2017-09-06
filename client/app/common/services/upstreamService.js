ae59ee140aae63059d6cd2a71553b50be68bc142
Jake Cross <jake.cross@thetrainline.com>
2017-09-06 15:42:29 +0100

    refactor the upstream service for client

--- client/app/common/services/upstreamService.js
+++ client/app/common/services/upstreamService.js
@@ -17,28 +17,36 @@
       return UpstreamConfig.getAll();
     }
 
+    /**
+     * @param {String} environmentName
+     * @return {Array : [ HostList [ Host, Host ]]}
+     */
     function dns(environmentName) {
       return UpstreamConfig.getAll()
-        .then(function (upstreams) {
-          return upstreams.filter(function (upstream) {
-            return upstream.Value.EnvironmentName === environmentName;
-          });
-        })
-        .then(function (upstreams) {
-          var results = [];
-          upstreams.forEach(function (upstream) {
-            var hosts = [];
-            upstream.Value.Hosts.forEach(function (host) {
-              hosts.push({
-                host: host.DnsName,
-                port: host.Port,
-                environment: upstream.Value.EnvironmentName
-              });
-            });
-            results.push(hosts);
-          });
-          return results;
-        });
+        .then(getUpstreamsThatMatchEnvironment.bind(environmentName))
+        .then(generateListOfHosts);
     }
+  }
+
+  function getUpstreamsThatMatchEnvironment(environmentName, upstreams) {
+    return upstreams.filter(function (upstream) {
+      return upstream.Value.EnvironmentName === environmentName;
+    });
+  }
+
+  function generateListOfHosts(upstreams) {
+    var results = [];
+    upstreams.forEach(function (upstream) {
+      var hosts = [];
+      upstream.Value.Hosts.forEach(function (host) {
+        hosts.push({
+          host: host.DnsName,
+          port: host.Port,
+          environment: upstream.Value.EnvironmentName
+        });
+      });
+      results.push(hosts);
+    });
+    return results;
   }
 }());
