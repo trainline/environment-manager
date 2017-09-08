ea3a779991a569701040eb548527ee3f53af32c1
Jake Cross <jake.cross@thetrainline.com>
2017-09-08 16:54:57 +0100

    collecting token from configuration file, sending that to client instances

--- server/modules/remoteCacheFlush.js
+++ server/modules/remoteCacheFlush.js
@@ -2,59 +2,127 @@
 
 let consulClient = require('modules/consul-client');
 let _ = require('lodash');
-let request = require('request');
+let request = require('request').defaults({ strictSSL: false });
 let logger = require('modules/logger');
+let configEnvironments = require('modules/data-access/configEnvironments');
+let config = require('config');
 
 function flush(environment, hosts) {
-  let clientInstance;
+  let consulClientInstance;
+  let token;
 
   return consulClient.create({ environment, promisify: true })
     .then(storeInstance)
+    .then(() => {
+      return getToken(environment)
+        .then((tokenValue) => {
+          token = tokenValue;
+        });
+    })
     .then(getServicesInEnvironment)
     .then(convertServiceObjectToListOfServices)
     .then(getNodesForServices)
     .then(createAddresses(hosts))
-    .then(sendRequestToAddresses)
+    .then((addresses) => { return sendRequestToAddresses(token, addresses); })
     .catch((e) => {
+      logger.error('Cache Reset Error: ', e);
       return { error: e.message };
     });
 
   function storeInstance(instance) {
-    clientInstance = instance;
+    consulClientInstance = instance;
   }
 
   function getServicesInEnvironment() {
-    return clientInstance.catalog.service.list();
+    return consulClientInstance.catalog.service.list();
   }
 
   function convertServiceObjectToListOfServices(services) {
-    return Object.keys(services).map(s => s);
+    if (services) return Object.keys(services).map(s => s);
+    else return [];
   }
 
   function getNodesForServices(serviceList) {
     // [ service: listOfNodesInService ]
     return Promise.all(serviceList.map((s) => {
-      return clientInstance.catalog.service.nodes(s);
+      return consulClientInstance.catalog.service.nodes(s);
     }));
   }
 }
+
+function stripPrefix(value) {
+  let prefixes = ['/^upstream_/', '/^slice_/'];
+  let result = '';
+  prefixes.forEach((p) => {
+    result = value.replace(p, '');
+  });
+  return result;
+}
+
 
 function createAddresses(hosts) {
   return (nodesLists) => {
     let addresses = [];
     _.flatten(hosts).forEach((host) => {
-      let node = _.flatten(nodesLists).find(n => n.ServiceName === host.host);
-      if (node) addresses.push(`https://${node.Address}:${host.port}/diagnostics/cachereset`);
+      let node = _.flatten(nodesLists).find((n) => {
+        // todo: Configuration String endpoint to store these ignore strings
+        return stripPrefix(n.ServiceName) === host.host;
+      });
+      if (node) {
+        let ip = node.Address;
+        addresses.push({
+          Address: `https://${ip}:${host.port}/diagnostics/cachereset`,
+          Host: host.host,
+          ServiceName: node.ServiceName
+        });
+      }
     });
     return addresses;
   };
 }
 
-function sendRequestToAddresses(addresses) {
+function sendRequestToAddresses(token, addresses) {
   addresses.forEach((address) => {
-    logger.info(`[CacheReset::Sent] to ${address}`);
-    request.post(address);
+    let options = {
+      method: 'POST',
+      uri: address.Address,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: {
+        token
+      },
+      json: true
+    };
+
+    request.post(options, (error, response, body) => {
+      if (response && response.statusCode === 401) {
+        logger.error('401 received: ', JSON.stringify(options));
+      }
+      if (response && response.statusCode === 200) {
+        logger.info('200 received: ', JSON.stringify(options));
+      }
+    });
   });
+}
+
+function getToken(EnvironmentName) {
+  return configEnvironments.get({ EnvironmentName })
+    .then(getEnvironmentTypeValue)
+    .then(getCacheResetKeyForEnvironment);
+
+  function getEnvironmentTypeValue(environment) {
+    return environment.EnvironmentType;
+  }
+
+  function getCacheResetKeyForEnvironment(environmentType) {
+    try {
+      let value = config.getUserValue('local').CacheReset[environmentType];
+      return value;
+    } catch (e) {
+      return `[No Cache Reset Key Found] :: ${environmentType}`;
+    }
+  }
 }
 
 module.exports = {
--- server/test/modules/remoteCacheFlushTest.js
+++ server/test/modules/remoteCacheFlushTest.js
@@ -5,7 +5,7 @@ const rewire = require('rewire');
 const assert = require('assert');
 const sinon = require('sinon');
 
-describe('remoteCacheFlush', () => {
+describe.only('remoteCacheFlush', () => {
   let sut;
   let MockRequest;
 
@@ -18,13 +18,35 @@ describe('remoteCacheFlush', () => {
     };
     // eslint-disable-next-line no-underscore-dangle
     sut.__set__('request', MockRequest);
+    // eslint-disable-next-line no-underscore-dangle
+    sut.__set__('configEnvironments', {
+      get: sinon.stub().returns(Promise.resolve(
+        { EnvironmentType: 'cluster' }
+      ))
+    });
+    // eslint-disable-next-line no-underscore-dangle
+    sut.__set__('config', {
+      getUserValue: sinon.stub().returns({
+        CacheReset: {
+          cluster: 'aaa'
+        }
+      })
+    });
   });
 
   it('should make a post request for each address found', (done) => {
     sut.flush('c50', MockData().Hosts)
       .then((addresses) => {
         MockData().Expectations.Addresses.forEach((address) => {
-          assert(MockRequest.post.calledWith(address));
+          assert(MockRequest.post.calledWith({
+            method: 'POST',
+            uri: address,
+            headers: { 'Content-Type': 'application/json' },
+            json: true,
+            body: {
+              token: 'aaa'
+            }
+          }));
         });
         done();
       })
@@ -63,11 +85,15 @@ function MockData() {
       [
         { host: 'c50-serviceDeLaCheese-green', port: 3333, environment: 'c50' },
         { host: 'c50-serviceDeLaCheese-blue', port: 4444, environment: 'c50' }
-      ]
+      ],
+      [{ host: 'c50-upstream-blue', port: 1212, environment: 'c50' }],
+      [{ host: 'c50-slice-blue', port: 3333, environment: 'c50' }]
     ],
     Services: {
       'c50-serviceSuper-green': [],
-      'c50-serviceDeLaCheese-blue': []
+      'c50-serviceDeLaCheese-blue': [],
+      'c50-upstream-blue': [],
+      'c50-slice-blue': []
     },
     Nodes: [
       {
@@ -77,12 +103,22 @@ function MockData() {
       {
         ServiceName: 'c50-serviceDeLaCheese-blue',
         Address: '1.3.5.7'
+      },
+      {
+        ServiceName: 'c50-upstream-blue',
+        Address: '1.2.1.2'
+      },
+      {
+        ServiceName: 'c50-slice-blue',
+        Address: '3.3.3.3'
       }
     ],
     Expectations: {
       Addresses: [
         'https://1.1.1.1:1111/diagnostics/cachereset',
-        'https://1.3.5.7:4444/diagnostics/cachereset'
+        'https://1.3.5.7:4444/diagnostics/cachereset',
+        'https://1.2.1.2:1212/diagnostics/cachereset',
+        'https://3.3.3.3:3333/diagnostics/cachereset'
       ]
     }
   };
