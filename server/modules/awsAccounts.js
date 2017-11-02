bd08dd006c2cee8ee192b4df8d97ae169b76a1d3
Merlin Taylor <code.merlin.taylor@gmail.com>
2017-11-02 11:24:25 +0000

    find AWS account by number

--- server/modules/awsAccounts.js
+++ server/modules/awsAccounts.js
@@ -2,24 +2,28 @@
 
 'use strict';
 
+let assert = require('assert');
 let ResourceNotFoundError = require('./errors/ResourceNotFoundError.class');
 let { scan } = require('./data-access/accounts');
 
 function getByName(accountName) {
-  let matches = val => accountName.toLowerCase() === val.toString().toLowerCase();
+  let matches = val => `${accountName}`.toLowerCase() === `${val}`.toLowerCase();
 
-  return getAllAccounts().then((accounts) => {
-    let matchingAccounts = [].concat(
-      accounts.filter(account => matches(account.AccountNumber)),
-      accounts.filter(account => matches(account.AccountName))
-    );
+  return Promise.resolve()
+    .then(() => { assert(typeof accountName === 'number' || typeof accountName === 'string', `${accountName}`); })
+    .then(getAllAccounts)
+    .then((accounts) => {
+      let matchingAccounts = [].concat(
+        accounts.filter(account => matches(account.AccountNumber)),
+        accounts.filter(account => matches(account.AccountName))
+      );
 
-    if (matchingAccounts.length > 0) {
-      return matchingAccounts[0];
-    } else {
-      throw new ResourceNotFoundError(`AWS account ${accountName} not found`);
-    }
-  });
+      if (matchingAccounts.length > 0) {
+        return matchingAccounts[0];
+      } else {
+        throw new ResourceNotFoundError(`AWS account ${accountName} not found`);
+      }
+    });
 }
 
 function getAMIsharingAccounts() {
