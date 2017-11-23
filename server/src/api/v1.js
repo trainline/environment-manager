470f13e72dc72921c61789b5b70eb843d00df959
Merlin Taylor <code.merlin.taylor@gmail.com>
2017-11-23 15:27:10 +0000

    import API controllers as opposed to using swagger-tools directory-based convention

--- server/src/api/v1.js
+++ server/src/api/v1.js
@@ -3,13 +3,12 @@
 'use strict';
 
 const swaggerTools = require('swagger-tools');
-const fs = require('fs');
 const config = require('../config');
-const path = require('path');
 const swaggerAuthorizer = require('../modules/express-middleware/swaggerAuthorizerMiddleware');
 const swaggerNewRelic = require('../modules/express-middleware/swaggerNewRelicMiddleware');
 const defaultErrorHandler = require('./error-handler/defaultErrorHandler');
 const apiSpec = require('./swagger-doc');
+const controllers = require('./controllers');
 
 const NODE_ENV = process.env.NODE_ENV;
 const API_BASE_PATH = apiSpec.basePath;
@@ -19,48 +18,7 @@ if (config.get('IS_PRODUCTION') === false) {
   apiSpec.schemes = ['http'];
 }
 
-function getControllerDirectories(root) {
-  let controllerNames = {};
-  function loop(dir) {
-    let fsEntries = fs.readdirSync(path.resolve(dir)).map((name) => {
-      let fullname = path.resolve(dir, name);
-      let stat = fs.statSync(fullname);
-      return {
-        fullname,
-        isDirectory: stat.isDirectory(),
-        isFile: stat.isFile()
-      };
-    });
-
-    let dirs = fsEntries.filter(f => f.isDirectory).map(f => f.fullname);
-    let files = fsEntries.filter(f => f.isFile).map(f => f.fullname);
-
-    if (files.length > 0 && dirs.length > 0) {
-      // eslint-disable-next-line max-len
-      throw new Error(`Controller directories must contain either controller files or subdirectories but "${dir}" contains both.`);
-    }
-
-    files.forEach((f) => {
-      let basename = path.basename(f);
-      if (controllerNames[basename]) {
-        // eslint-disable-next-line max-len
-        throw new Error(`Controller names must be unique but "${basename}" was found at "${f}" and at "${controllerNames[basename]}".`);
-      } else {
-        controllerNames[basename] = f;
-      }
-    });
-
-    if (dirs.length === 0) {
-      return [dir];
-    }
-    return Array.prototype.concat.apply([], dirs.map(d => loop(d)));
-  }
-  return loop(root);
-}
-
-let swaggerOptions = {
-  controllers: getControllerDirectories(path.resolve(__dirname, 'controllers'))
-};
+let swaggerOptions = { controllers: controllers() };
 
 function setup() {
   return new Promise((resolve, reject) => {
