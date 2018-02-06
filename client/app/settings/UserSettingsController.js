43ee77b7973454d2798faccc5a996f3786ad5dfa
jake-cross <jake.cross@thetrainline.com>
2018-02-06 11:04:31 +0000

    resolve linting issues

--- client/app/settings/UserSettingsController.js
+++ client/app/settings/UserSettingsController.js
@@ -1,50 +1,51 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('EnvironmentManager.settings')
-        .controller('UserSettingsController', UserSettingsController);
+  angular
+    .module('EnvironmentManager.settings')
+    .controller('UserSettingsController', UserSettingsController);
 
-    UserSettingsController.$inject = ['$uibModal', '$uibModalInstance', 'localstorageservice'];
+  UserSettingsController.$inject = ['$uibModal', '$uibModalInstance', 'localstorageservice'];
 
-    function UserSettingsController($uibModal, $uibModalInstance, localstorageservice) {
-        var vm = this;
+  function UserSettingsController($uibModal, $uibModalInstance, localstorageservice) {
+    var vm = this;
 
-        vm.title = 'User Settings';
-        vm.loadSettings = loadSettings;
-        vm.save = save;
-        vm.closeAndSave = closeAndSave;
-        vm.close = close;
+    vm.title = 'User Settings';
+    vm.loadSettings = loadSettings;
+    vm.save = save;
+    vm.closeAndSave = closeAndSave;
+    vm.close = close;
 
-        vm.settings = {};
+    vm.settings = {};
 
-        loadSettings();
+    loadSettings();
 
-        function save() {
-            _saveEnvironments();
-        }
-
-        function closeAndSave() {
-            save();
-            $uibModalInstance.close();
-        }
-
-        function close() {
-            $uibModalInstance.close();
-        }
-
-        function loadSettings() {
-            vm.settings.environments = localstorageservice.get('em-settings-environments');
-        }
-
-        function _saveEnvironments() {
-            var providedEnvironments = vm.settings.environments;
-            var listOfEnvironments = providedEnvironments.split(',').map(function (e) {
-                return e.trim();
-            });
-            var environmentsReadyToSave = listOfEnvironments.join(',');
-            vm.settings.environments = environmentsReadyToSave;
-            localstorageservice.set('em-settings-environments', environmentsReadyToSave);
-        }
+    function save() {
+      _saveEnvironments();
     }
+
+    function closeAndSave() {
+      save();
+      $uibModalInstance.close();
+    }
+
+    function close() {
+      $uibModalInstance.close();
+    }
+
+    function loadSettings() {
+      vm.settings.environments = localstorageservice.get('em-settings-environments');
+    }
+
+    /* eslint-disable no-underscore-dangle */
+    function _saveEnvironments() {
+      var providedEnvironments = vm.settings.environments;
+      var listOfEnvironments = providedEnvironments.split(',').map(function (e) {
+        return e.trim();
+      });
+      var environmentsReadyToSave = listOfEnvironments.join(',');
+      vm.settings.environments = environmentsReadyToSave;
+      localstorageservice.set('em-settings-environments', environmentsReadyToSave);
+    }
+  }
 }());
